d98e9bfdfbf9107ca5a95aabc9583489bc6afcfb
will cohen <willc@mojotech.com>
2020-05-29 15:55:27 -0400

    add a base style system

---
+++ src/ui/base.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import emotionStyled, { CreateStyled } from "@emotion/styled";
+import { Global, css } from "@emotion/core";
+import {
+  ThemeProvider as EmotionThemeProvider,
+  useTheme as useEmotionTheme
+} from "emotion-theming";
+import { buildTheme, Theme } from "./theme";
+
+export const styled: CreateStyled<Theme> = emotionStyled;
+
+export type ThemeProp = { theme: Theme };
+
+export const useTheme = () => useEmotionTheme<Theme>();
+
+const GlobalStyles = () => {
+  const theme = useTheme();
+  return (
+    <Global
+      styles={css`
+        body {
+          background: ${theme.colors.brand.background};
+          color: ${theme.colors.brand.text};
+          margin: 0;
+        }
+      `}
+    />
+  );
+};
+
+export const ThemeProvider = (props: any) => {
+  return (
+    <EmotionThemeProvider theme={buildTheme(false)}>
+      {props.children}
+      <GlobalStyles />
+    </EmotionThemeProvider>
+  );
+};
+
+const textStyles = ({ theme }: { theme: Theme }) => `
+color: ${theme.colors.brand.text};
+font-family: ${theme.typefaces.sansSerif};
+.code {
+  font-family: ${theme.typefaces.mono}
+}
+h1, h2, h3, h4, h5, h6 {
+  font-family: ${theme.typefaces.serif}
+}
+h1 {
+  font-size: ${theme.fontSizes[8]}px;
+}
+h2 {
+  font-size: ${theme.fontSizes[7]}px;
+}
+h3 {
+  font-size: ${theme.fontSizes[6]}px;
+}
+h4 {
+  font-size: ${theme.fontSizes[5]}px;
+}
+h5 {
+  font-size: ${theme.fontSizes[4]}px;
+}
+h6 {
+  font-size: ${theme.fontSizes[3]}px;
+}
+.f1 {
+  font-size: ${theme.fontSizes[0]}px;
+}
+.f2 {
+  font-size: ${theme.fontSizes[1]}px;
+}
+.f3 {
+  font-size: ${theme.fontSizes[2]}px;
+}
+.f4 {
+  font-size: ${theme.fontSizes[3]}px;
+}
+.f5 {
+  font-size: ${theme.fontSizes[4]}px;
+}
+.f6 {
+  font-size: ${theme.fontSizes[5]}px;
+}
+`;
+
+export const Div = styled.div(textStyles, ({ theme }) => ``);
+
+export const Page = styled(Div)(
+  ({ theme }) => `
+  min-height: 100vh;
+  width: ${theme.size[1]};
+`
+);
