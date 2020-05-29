ccacbcc942e8c992b2a06eb72eb0ee5edb9f4d59
will cohen <willc@mojotech.com>
2020-05-29 17:13:59 -0400

    netlify identity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
+import dynamic from 'next/dynamic';
 import { attributes, react as Stuff } from "content/stuff.md";
+
+dynamic(() => import('utils/netlify-login'), {ssr: false})
 
 export default () => {
   const { title, cats } = attributes;
   return (
     <div>
-        <h1>Hello World</h1>
-        <div>{title}</div>
-        <ul>
-            {cats.map((cat, k) => (
-                <li key={k}>
-                    <h2>{cat.name}</h2>
-                    <p>{cat.description}</p>
-                </li>
-            ))}
-        </ul>
+      <h1>Hello World</h1>
+      <div>{title}</div>
+      <ul>
+        {cats.map((cat, k) => (
+          <li key={k}>
+            <h2>{cat.name}</h2>
+            <p>{cat.description}</p>
+          </li>
+        ))}
+      </ul>
       <Stuff />
     </div>
   );
