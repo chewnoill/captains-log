7292865dce34caf650db21a51bcf9cbb9505e63b
will cohen <willc@mojotech.com>
2020-05-29 16:30:49 -0400

    setup netlify cms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-export default () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+import { attributes, react as Stuff } from "content/stuff.md";
+
+export default () => {
+  const { title, cats } = attributes;
+  return (
+    <div>
+        <h1>Hello World</h1>
+        <div>{title}</div>
+        <ul>
+            {cats.map((cat, k) => (
+                <li key={k}>
+                    <h2>{cat.name}</h2>
+                    <p>{cat.description}</p>
+                </li>
+            ))}
+        </ul>
+      <Stuff />
+    </div>
+  );
+};
