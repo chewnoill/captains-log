637bca91463b1b0bcb0079a106c59a2b558a00d6
will cohen <willc@mojotech.com>
2020-05-29 20:30:49 +0000

    setup netlify cms

--- src/components/cms/index.tsx
+++ src/components/cms/index.tsx
@@ -1,43 +1,22 @@
-import { useState, useEffect, useRef, Component } from "react";
+import { useState, useEffect, createElement, Suspense } from "react";
 import Head from "next/head";
 import CMS from "netlify-cms-app";
 import Page from "components/page";
 import MDX from "@mdx-js/runtime";
 import { ThemeProvider } from "ui";
-import createCache from "@emotion/cache";
-import { CacheProvider } from "@emotion/core";
 
-class CSSInjector extends Component {
-  cache: any;
-  constructor(...args) {
-    super(...args);
-    const iframe = document.getElementsByTagName("iframe")[0];
-    const iframeHead = iframe.contentDocument.head;
-    this.cache = createCache({ container: iframeHead });
-  }
 
-  render() {
-    return (
-      <CacheProvider value={this.cache}>{this.props.children}</CacheProvider>
-    );
-  }
-}
-
-const ThemedPage = (args: any) => {
-  const { body, ...props } = args.entry.getIn(["data"]).toJS();
+const ThemedPage = (props: any) => {
+  const data = props.entry.getIn(["data"]).toJS();
   return (
-    <CSSInjector>
-      <ThemeProvider>
-        <Page body={() => <MDX>{body}</MDX>} {...props} />
-      </ThemeProvider>
-    </CSSInjector>
+    <ThemeProvider>
+      <Page title={data.title} body={() => <MDX>{data.body}</MDX>} />
+    </ThemeProvider>
   );
 };
 
 export default () => {
   CMS.init();
-  CMS.registerPreviewTemplate("root", ThemedPage);
-  CMS.registerPreviewTemplate("blog", ThemedPage);
   CMS.registerPreviewTemplate("index", ThemedPage);
   return (
     <Head>
