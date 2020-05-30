3909dc62153185932c0a5b715b5dcad42c2c1b2e
will cohen <willc@mojotech.com>
2020-05-30 18:19:55 -0400

    Update content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,16 @@
 import dynamic from "next/dynamic";
-import Content, { title, cats } from "content/index.md";
+import Content, { title } from "content/index.md";
+import { styled } from "ui";
 
 dynamic(() => import("utils/netlify-login"), { ssr: false });
 
+const StyledIndex = styled.div();
+
 export default () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <div>{title}</div>
-      <ul>
-        {cats.map((cat, k) => (
-          <li key={k}>
-            <h2>{cat.name}</h2>
-            <p>{cat.description}</p>
-          </li>
-        ))}
-      </ul>
+    <StyledIndex>
+      <h1>{title}</h1>
       <Content />
-    </div>
+    </StyledIndex>
   );
 };
