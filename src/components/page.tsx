b41b42eef91a8eef4ba2fe3354fc09a3bfbf36da
will cohen <willc@mojotech.com>
2020-06-03 09:20:15 -0400

    add some page styling

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 import { styled } from "ui";
 
-export const StyledIndex = styled.div();
+export const StyledIndex = styled.div(
+  ({ theme }) => `
+margin:${theme.size[4]}px;
+
+article {
+  max-width:${theme.measures[1]};
+}
+
+blockquote {
+  background: ${theme.colors["black-10"]};
+  padding: ${theme.space[2]}px;
+}
+
+heading {
+  font-weight: bold;
+  font-size: ${theme.fontSizes[4]}px 
+}
+
+.narrow-text {
+  max-width:${theme.measures[0]};
+}
+
+.standard-text {
+  max-width:${theme.measures[1]};
+}
+
+.wide-text {
+  max-width:${theme.measures[2]};
+}
+`
+);
 
 export interface Props {
   title: string;
@@ -11,7 +41,7 @@ export interface Props {
 export default ({ title, body: Body }: Props) => {
   return (
     <StyledIndex>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <Body />
     </StyledIndex>
   );
