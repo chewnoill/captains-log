dd885d158f546a2fe714682b06856d7e7ecde37f
will <chewnoill@gmail.com>
2020-06-08 16:32:28 -0400

    restyle the page

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -11,15 +11,23 @@ align-items:center;
   text-align: center;
 }
 
+code {
+  font-size: ${theme.fontSizes[2]}px;
+  color: ${theme.colors["black-60"]};
+  font-family: ${theme.typefaces.mono};
+}
+
 article {
+  font-size: ${theme.fontSizes[3]}px;
+  line-height: ${theme.lineHeights[1]};
   max-width:${theme.measures[2]};
 }
 
 section {
   max-width:${theme.measures[1]};
   background: ${theme.colors["black-10"]};
-  padding: ${theme.space[2]}px;
-  margin: ${theme.space[2]}px;
+  padding: ${theme.space[5]}px;
+  margin-bottom: ${theme.space[5]}px;
 }
 
 blockquote {
@@ -53,16 +61,6 @@ heading {
   max-width:${theme.measures[2]};
 }
 
-.footnotes {
-  font-size: ${theme.fontSizes[0]}px;
-  width: ${theme.size[1]};
-  p {
-    display: inline;
-  }
-  li {
-    max-width:${theme.measures[1]};
-  }
-}
 `
 );
 
