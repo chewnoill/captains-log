064b9a46aef7df1e683f1d6c1138a3df59e1d18e
will <chewnoill@gmail.com>
2020-06-08 16:34:03 -0400

    add emojo and image remark plugins

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -2,9 +2,13 @@ import MDX from "@mdx-js/runtime";
 import { MDXProvider } from "@mdx-js/react";
 import Prompt from "components/prompt";
 import Code from "components/code";
+import remarkImages from "remark-images";
+import remarkEmoji from "remark-emoji";
 
 export default ({ children, scope }: { children: string; scope: any }) => (
   <MDXProvider components={{ Prompt, code: Code }}>
-    <MDX scope={scope}>{children}</MDX>
+    <MDX remarkPlugins={[remarkImages, remarkEmoji]} scope={scope}>
+      {children}
+    </MDX>
   </MDXProvider>
 );
