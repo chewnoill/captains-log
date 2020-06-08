6b44830db4c9f5d3265747ccf0ebb19fabebb921
will <will@penguin>
2020-06-07 21:32:20 -0400

    add syntax highlighter

---
+++ src/components/code.tsx
@@ -0,0 +1,16 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+
+interface Props {
+    className: string;
+    children: string;
+}
+
+export default function Code({ className, children }: Props) {
+  const lang = className?className.split("-")[1]:'text';
+  return (
+    <SyntaxHighlighter language={lang} style={docco}>
+      {children}
+    </SyntaxHighlighter>
+  );
+}
--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,9 +1,10 @@
 import MDX from "@mdx-js/runtime";
 import { MDXProvider } from "@mdx-js/react";
 import Prompt from "components/prompt";
+import Code from "components/code";
 
 export default ({ children, scope }: { children: string; scope: any }) => (
-  <MDXProvider components={{ Prompt }}>
+  <MDXProvider components={{ Prompt, code: Code }}>
     <MDX scope={scope}>{children}</MDX>
   </MDXProvider>
 );
