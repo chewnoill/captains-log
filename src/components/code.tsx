e3b42367aa0272d50226facb1698a632d44a1710
will cohen <willc@mojotech.com>
2020-06-15 10:53:42 -0400

    fix formatting

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -2,8 +2,8 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 interface Props {
-    className: string;
-    children: string;
+  className: string;
+  children: string;
 }
 
 export default function Code({ className, children }: Props) {
