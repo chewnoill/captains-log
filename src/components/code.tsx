74e353aaa159d9d90b332d8c7393fbe84a100716
will cohen <willc@mojotech.com>
2020-06-15 10:55:03 -0400

    add mermaid code block
    mermaid wants access to the browser and can't be staticly rendered

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,5 +1,7 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import dynamic from "next/dynamic";
+const Mermaid = dynamic(() => import("components/mermaid"), { ssr: false });
 
 interface Props {
   className: string;
@@ -7,10 +9,15 @@ interface Props {
 }
 
 export default function Code({ className, children }: Props) {
-  const lang = className?className.split("-")[1]:'text';
-  return (
-    <SyntaxHighlighter language={lang} style={docco}>
-      {children}
-    </SyntaxHighlighter>
-  );
+  const lang = className ? className.split("-")[1] : "text";
+  switch (lang) {
+    case "mermaid":
+      return <Mermaid>{children}</Mermaid>;
+    default:
+      return (
+        <SyntaxHighlighter language={lang} style={docco}>
+          {children}
+        </SyntaxHighlighter>
+      );
+  }
 }
